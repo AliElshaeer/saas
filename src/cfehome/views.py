092d9404c0e53e68b626f1ef47a814cee0a5dbef
AliElshaeer <alielshaeer131@gmail.com>
2025-08-18 00:30:51 +0300

    solve the error in railway

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -10,11 +10,15 @@ def home_view(request, *args, **kwargs):
     # print(this_dir)
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = (page_qs.count() * 100.0) / qs.count()
+    except:
+        percent = 0
     my_title = "my Page"
     my_context = {
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
-        "percent": (page_qs.count() * 100) / qs.count(),
+        "percent": percent,
         "total_visit_count": qs.count(),
     }
     path = request.path
